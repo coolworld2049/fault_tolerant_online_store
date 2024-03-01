9aafe5d428c3a13cf88a46f24eb67d35b9530049
Artem Bobylev <temka2.2@mail.ru>
2024-03-01 18:35:06 +0300

    upd poetry_export.sh

--- scripts/poetry_export.sh
+++ scripts/poetry_export.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-poetry export --without-hashes --format=requirements.txt > requirements.txt
-git add requirements.txt
+if command -v poetry &> /dev/null; then
+  poetry export --without-hashes --format=requirements.txt > requirements.txt
+  git add requirements.txt
+else
+    echo "Poetry is not installed. Please install it from https://python-poetry.org/docs/."
+fi
